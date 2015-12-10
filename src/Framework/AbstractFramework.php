5396b989a2a6e69597adde1c600c681e36047a00
UCSDMath <ucsdmath.project@gmail.com>
2015-12-10 09:49:24 -0800

    Update

--- src/Framework/AbstractFramework.php
+++ src/Framework/AbstractFramework.php
@@ -1,72 +1,1 @@
-<?php
-
-/*
- * This file is part of the UCSDMath package.
- *
- * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace UCSDMath\Framework;
-
-/**
- * AbstractFramework provides an abstract base class implementation of {@link FrameworkInterface}.
- * Primarily, this services the fundamental implementations for all Framework classes.
- *
- * Method list:
- *
- * @method FrameworkInterface __construct();
- * @method void __destruct();
- *
- * @author Daryl Eisner <deisner@ucsd.edu>
- */
-abstract class AbstractFramework implements FrameworkInterface
-{
-    /**
-     * Constants.
-     *
-     * @var string VERSION  A version number
-     *
-     * @api
-     */
-    const VERSION = '1.5.0';
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Properties.
-     *
-     * @var    array              $storageRegister  A set of validation stored data elements
-     * @static FrameworkInterface $instance         A FrameworkInterface instance
-     * @static integer            $objectCount      A FrameworkInterface instance count
-     */
-    protected $storageRegister = array();
-    protected static $instance = null;
-    protected static $objectCount = 0;
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Constructor.
-     *
-     * @api
-     */
-    public function __construct()
-    {
-        static::$objectCount++;
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Destructor.
-     *
-     * @api
-     */
-    public function __destruct()
-    {
-        static::$objectCount--;
-    }
-}
+<?php/* * This file is part of the UCSDMath package. * * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu> * * For the full copyright and license information, please view the LICENSE * file that was distributed with this source code. */namespace UCSDMath\Framework;/** * AbstractFramework provides an abstract base class implementation of {@link FrameworkInterface}. * Primarily, this services the fundamental implementations for all Framework classes. * * Method list: * * @method FrameworkInterface __construct(); * @method void __destruct(); * * @author Daryl Eisner <deisner@ucsd.edu> */abstract class AbstractFramework implements FrameworkInterface{    /**     * Constants.     *     * @var string VERSION  A version number     *     * @api     */    const VERSION = '1.5.0';    // --------------------------------------------------------------------------    /**     * Properties.     *     * @var    array              $storageRegister  A set of validation stored data elements     * @static FrameworkInterface $instance         A FrameworkInterface instance     * @static integer            $objectCount      A FrameworkInterface instance count     */    protected $storageRegister = array();    protected static $instance = null;    protected static $objectCount = 0;    // --------------------------------------------------------------------------    /**     * Constructor.     *     * @api     */    public function __construct()    {        static::$objectCount++;    }    // --------------------------------------------------------------------------    /**     * Destructor.     *     * @api     */    public function __destruct()    {        static::$objectCount--;    }}
--- src/Framework/Listener/ControllerListenerInterface.php
+++ src/Framework/Listener/ControllerListenerInterface.php
@@ -1,24 +1,1 @@
-<?php
-
-/*
- * This file is part of the UCSDMath package.
- *
- * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace UCSDMath\Framework\Listener;
-
-/**
- * ControllerListenerInterface is the interface implemented by all ControllerListener classes.
- *
- * @author Daryl Eisner <deisner@ucsd.edu>
- */
-interface ControllerListenerInterface
-{
-    /**
-     * Constants.
-     */
-}
+<?php/* * This file is part of the UCSDMath package. * * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu> * * For the full copyright and license information, please view the LICENSE * file that was distributed with this source code. */namespace UCSDMath\Framework\Listener;/** * ControllerListenerInterface is the interface implemented by all ControllerListener classes. * * @author Daryl Eisner <deisner@ucsd.edu> */interface ControllerListenerInterface{    /**     * Constants.     */}
