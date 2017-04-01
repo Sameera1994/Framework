dff7bb7de187e1a51a727cbcace1375f572e405f
UCSDMath <UCSDMath.Project@gmail.com>
2017-03-31 17:03:54 -0700

    Update

--- src/Framework/Listener/MathlinkControllerListener.php
+++ src/Framework/Listener/MathlinkControllerListener.php
@@ -41,7 +41,7 @@ class MathlinkControllerListener implements ControllerListenerInterface
      *
      * @api
      */
-    public const VERSION = '1.17.0';
+    public const VERSION = '1.18.0';
 
     //--------------------------------------------------------------------------
 
