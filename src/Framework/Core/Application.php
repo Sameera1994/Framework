cf96f53cfbd85ca9ebceb4b888e1c7d092c3252b
UCSDMath <ucsdmath.project@gmail.com>
2017-08-13 12:33:45 -0700

    Update

--- src/Framework/Core/Application.php
+++ src/Framework/Core/Application.php
@@ -74,7 +74,7 @@ class Application implements HttpKernelInterface, ApplicationInterface
      *
      * @api
      */
-    public const VERSION = '1.24.0';
+    public const VERSION = '1.25.0';
 
     //--------------------------------------------------------------------------
 
--- src/Framework/Core/BaseController.php
+++ src/Framework/Core/BaseController.php
@@ -35,7 +35,7 @@ class BaseController extends Controller
      *
      * @api
      */
-    public const VERSION = '1.24.0';
+    public const VERSION = '1.25.0';
 
     //--------------------------------------------------------------------------
 
